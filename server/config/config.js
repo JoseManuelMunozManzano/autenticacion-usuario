b72a0b4528d8662b2e8369587730b5494abb7148
Neimerc <Neil_Mercury@yahoo.es>
2020-12-04 20:34:09 +0100

    generar un JWT

--- server/config/config.js
+++ server/config/config.js
@@ -8,9 +8,25 @@ process.env.PORT = process.env.PORT || 3000;
 // =====================================
 // Entorno
 // =====================================
-// En Heroku cogerá en NODE_ENV y en local el 'dev'
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
+// =====================================
+// Vencimiento del Token
+// =====================================
+// 60 segundos
+// 60 minutos
+// 24 horas
+// 30 días
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+// =====================================
+// SEED de autenticación
+// =====================================
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
+
+// =====================================
+// Base de datos
+// =====================================
 if (process.env.NODE_ENV === 'dev') {
   process.env.DB_URL = process.env.DB_Local_URL;
   //process.env.DB_URL = process.env.MONGO_URI;
