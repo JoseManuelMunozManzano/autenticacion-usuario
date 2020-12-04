098942102c4efd01a7d710ac593694329991915b
Neimerc <Neil_Mercury@yahoo.es>
2020-12-04 19:31:49 +0100

    Ordenando las rutas en nuestro servidor

---
+++ server/routes/index.js
@@ -0,0 +1,9 @@
+// Definición de todas nuestras rutas
+const express = require('express');
+
+const app = express();
+
+app.use(require('./usuario'));
+app.use(require('./login'));
+
+module.exports = app;
